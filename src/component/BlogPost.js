a25d802692601ad195afc48c1dca1cfc8e85732c
leberSmeaton <77179232+leberSmeaton@users.noreply.github.com>
2021-11-08 11:58:55 +1100

    Added Post state and useEffect, and error handling on BlogPost

--- src/component/BlogPost.js
+++ src/component/BlogPost.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { capitalize } from '../utils/stringUtils'
 import Moment from 'react-moment'
+import { getBlogPost } from '../services/blogPostServices'
 
 export const BlogPost = (props) => {
-  const {post} = props
+  // const {post} = props
+  const [post,setPost] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    getBlogPost()
+    .then(post => setPost(post))
+    .catch(error => console.log(error))
+    .finally(setLoading(false))
+  },[])
+  
+  if(!post){
+    return(
+      <p>Oops, couldn't find your post.</p>
+    )
+  }
+
   return (
     <>
       <h1>{post.title}</h1>
--- src/services/blogPostServices.js
+++ src/services/blogPostServices.js
@@ -1,10 +1,17 @@
 import posts from '../data/posts';
 
 export const getBlogPosts = () => {
-  
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(posts)
+    }, 500)
+  })
+}
+
+export const getBlogPost = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(posts.find(post => post.id === id))
     }, 500)
   })
 }
